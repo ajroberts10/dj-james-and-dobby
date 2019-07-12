1ae3051aedcef9316b08944388f2823c95e51d11
Adam Roberts <adam.roberts@bbc.co.uk>
2019-07-12 21:54:18 +0100

    fix promoCard styling issue

--- src/components/PromoCard/PromoCard.jsx
+++ src/components/PromoCard/PromoCard.jsx
@@ -15,7 +15,7 @@ class PromoCard extends Component {
                             <div className="promoCard__content-header">
                                 <h2 className="featurette-heading display-5">About Us</h2>
                             </div>
-                            <div className="promoCard__content-text lead">
+                            <div className="promoCard__content-text">
                                 <p>
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                                 </p>
