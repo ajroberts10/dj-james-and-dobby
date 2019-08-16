5fb59d3e72e30e58cbdf16c5c8bdf600bb66c498
Adam Roberts <adam.roberts@bbc.co.uk>
2019-08-16 19:33:22 +0100

    form test

--- src/components/ContactModal/ContactModal.jsx
+++ src/components/ContactModal/ContactModal.jsx
@@ -2,8 +2,35 @@ import React, { Component } from 'react';
 
 import './ContactModal.scss';
 
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+}
+
 class ContactModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { email: "", message: "" };
+    }
+
+    handleSubmit = e => {
+        fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: encode({ "form-name": "contact", ...this.state })
+        })
+          .then(() => alert("Success!"))
+          .catch(error => alert(error));
+
+        e.preventDefault();
+    };
+
+    handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+
     render() {
+        const { email, message } = this.state;
         return (
             <div className="modal fade" id="contactModal" tabIndex="-1" role="dialog" aria-labelledby="contactModalTitle" aria-hidden="true">
                 <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
@@ -19,11 +46,26 @@ class ContactModal extends Component {
                             <input type="hidden" name="form-name" value="contact" />
                             <div className="form-group">
                                 <label>Email address</label>
-                                <input type="email" name="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                                <input
+                                    type="email"
+                                    name="email"
+                                    value={email}
+                                    onChange={this.handleChange}
+                                    className="form-control"
+                                    id="exampleFormControlInput1"
+                                    placeholder="name@example.com"
+                                />
                             </div>
                             <div className="form-group">
                                 <label>Example textarea</label>
-                                <textarea name="message" className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                <textarea
+                                    name="message"
+                                    value={message}
+                                    onChange={this.handleChange}
+                                    className="form-control"
+                                    id="exampleFormControlTextarea1"
+                                    rows="3"
+                                />
                             </div>
                         </form>
                     </div>
