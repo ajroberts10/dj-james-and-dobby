411a806b021ac2ea955d7c23fd60ed6937581ac9
Adam Roberts <adam.roberts@bbc.co.uk>
2019-08-16 19:03:51 +0100

    form test

--- src/components/ContactModal/ContactModal.jsx
+++ src/components/ContactModal/ContactModal.jsx
@@ -5,7 +5,7 @@ import './ContactModal.scss';
 class ContactModal extends Component {
     render() {
         return (
-            <div className="modal fade" id="contactModal" tabindex="-1" role="dialog" aria-labelledby="contactModalTitle" aria-hidden="true">
+            <div className="modal fade" id="contactModal" tabIndex="-1" role="dialog" aria-labelledby="contactModalTitle" aria-hidden="true">
                 <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
                     <div className="modal-content">
                     <div className="modal-header">
@@ -15,40 +15,20 @@ class ContactModal extends Component {
                         </button>
                     </div>
                     <div className="modal-body">
-                        <form>
-                            <div class="form-group">
-                                <label for="exampleFormControlInput1">Email address</label>
-                                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                        <form name="contact" method="POST" data-netlify="true">
+                            <div className="form-group">
+                                <label>Email address</label>
+                                <input type="email" name="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
                             </div>
-                            <div class="form-group">
-                                <label for="exampleFormControlSelect1">Example select</label>
-                                <select class="form-control" id="exampleFormControlSelect1">
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleFormControlSelect2">Example multiple select</label>
-                                <select multiple class="form-control" id="exampleFormControlSelect2">
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleFormControlTextarea1">Example textarea</label>
-                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            <div className="form-group">
+                                <label>Example textarea</label>
+                                <textarea name="message" className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                             </div>
                         </form>
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" className="btn btn-primary">Submit</button>
+                        <button type="submit" className="btn btn-primary">Submit</button>
                     </div>
                     </div>
                 </div>
