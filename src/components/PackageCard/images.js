9eaacaa2db2314352dc014af0124f672fbec8369
Adam Roberts <adam.roberts@bbc.co.uk>
2019-10-25 14:05:28 +0100

    adding images

--- src/components/PackageCard/images.js
+++ src/components/PackageCard/images.js
@@ -1,4 +1,5 @@
 export const bronze = [
+    "https://res.cloudinary.com/musicandcolour/image/upload/c_lfill,h_335,w_503/v1569261508/bronze/cover.jpg",
     "https://res.cloudinary.com/musicandcolour/image/upload/c_fill,w_400/v1569261508/bronze/1.jpg",
     "https://res.cloudinary.com/musicandcolour/image/upload/c_fill,w_400/v1569261508/bronze/2.jpg"
 ];
@@ -11,7 +12,8 @@ export const silver = [
 export const gold = [
     "https://res.cloudinary.com/musicandcolour/image/upload/c_scale,w_400/v1569263035/gold/1.jpg",
     "https://res.cloudinary.com/musicandcolour/image/upload/c_scale,w_400/v1569263035/gold/6.jpg",
-    "https://res.cloudinary.com/musicandcolour/image/upload/c_scale,w_400/v1569263035/gold/8.jpg"
+    "https://res.cloudinary.com/musicandcolour/image/upload/c_scale,w_400/v1569263035/gold/8.jpg",
+    "https://res.cloudinary.com/musicandcolour/image/upload/c_scale,h_335,w_500/v1572008519/gold/9.jpg"
 ]
 
 export const kidsParty = [
