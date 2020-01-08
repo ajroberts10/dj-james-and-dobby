465548895020f5f69fe31ab333294c1b15181ced
Adam Roberts <adam.roberts@bbc.co.uk>
2020-01-08 16:31:39 +0000

    update images

--- src/components/PackageCard/images.js
+++ src/components/PackageCard/images.js
@@ -1,17 +1,16 @@
 export const bronze = [
     "https://res.cloudinary.com/musicandcolour/image/upload/c_lfill,h_335,w_503/v1569261508/bronze/cover.jpg",
-    "https://res.cloudinary.com/musicandcolour/image/upload/c_fill,w_400/v1569261508/bronze/1.jpg",
-    "https://res.cloudinary.com/musicandcolour/image/upload/c_fill,w_400/v1569261508/bronze/2.jpg"
+    "https://res.cloudinary.com/musicandcolour/image/upload/c_lfill,h_335,w_503/v1578501015/bronze/Image_from_iOS_29.jpg"
 ];
 
 export const silver = [
+    "https://res.cloudinary.com/musicandcolour/image/upload/c_lfill,h_335,w_503/v1569263035/gold/10.jpg",
+    "https://res.cloudinary.com/musicandcolour/image/upload/c_lfill,h_335,w_503/v1569263035/gold/11.jpg",
     "https://res.cloudinary.com/musicandcolour/image/upload/c_fill,w_400/v1569262692/silver/7.jpg",
     "https://res.cloudinary.com/musicandcolour/image/upload/c_fill,w_400/v1569262692/silver/8.jpg"
 ];
 
 export const gold = [
-    "https://res.cloudinary.com/musicandcolour/image/upload/c_lfill,h_335,w_503/v1569263035/gold/10.jpg",
-    "https://res.cloudinary.com/musicandcolour/image/upload/c_lfill,h_335,w_503/v1569263035/gold/11.jpg",
     "https://res.cloudinary.com/musicandcolour/image/upload/c_scale,w_400/v1569263035/gold/1.jpg",
     "https://res.cloudinary.com/musicandcolour/image/upload/c_scale,w_400/v1569263035/gold/6.jpg",
     "https://res.cloudinary.com/musicandcolour/image/upload/c_scale,w_400/v1569263035/gold/8.jpg",
